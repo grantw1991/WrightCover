af392d8a14719e447dab00d4996eb6e7f21d7053
Grant <grant.wright@bglgroup.co.uk>
2017-11-27 09:55:00 +0000

    Initial commit - skeleton mvvm structure implemented

---
+++ MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrightCover
+{
+    public class MainViewModel
+    {
+        public string ButtonText { get; set; }
+
+        public MainViewModel()
+        {
+            ButtonText = "hello world";
+        }
+    }
+}
---
+++ MainWindowControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace WrightCover
+{
+    public partial class MainWindowControl : UserControl
+    {
+        public MainWindowControl()
+        {
+            DataContext = new MainViewModel();
+
+            this.InitializeComponent();
+        }
+    }
+}
